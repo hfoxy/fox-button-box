f38f5ea7cebc4de63b313df7a35df977d5be6abe
Harry Fox <1254033+hfoxy@users.noreply.github.com>
2024-09-22 21:00:02 +0100

    fix ordering for buttons

--- src/buttons.c
+++ src/buttons.c
@@ -31,11 +31,11 @@ button_t buttons[NUMBER_OF_BUTTONS] = {
 
 button_t buttons[NUMBER_OF_BUTTONS] = {
     [0].id = 1,
-    [0].gpio = 3,
+    [0].gpio = 4,
     [0].debounce_ms = 0,
 
     [1].id = 2,
-    [1].gpio = 4,
+    [1].gpio = 3,
     [1].debounce_ms = 0,
 
     [2].id = 3,
@@ -43,16 +43,12 @@ button_t buttons[NUMBER_OF_BUTTONS] = {
     [2].debounce_ms = 0,
 
     [3].id = 4,
-    [3].gpio = 6,
+    [3].gpio = 7,
     [3].debounce_ms = 0,
 
     [4].id = 5,
-    [4].gpio = 7,
+    [4].gpio = 6,
     [4].debounce_ms = 0,
-
-    [5].id = 6,
-    [5].gpio = 8,
-    [5].debounce_ms = 0,
 };
 
 void buttons_Initialise(void) {
@@ -70,7 +66,7 @@ void buttons_GetState(button_t *buttons) {
 uint8_t buttons_GetShortState(void) {
     uint8_t result = 0;
     for(int i = 0; i < NUMBER_OF_BUTTONS; i++) {
-        result = button_GetState(&buttons[i]) << i;
+        result = result | (button_GetState(&buttons[i]) << i);
     }
     return result;
 }
